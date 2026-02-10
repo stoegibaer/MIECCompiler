28b00d1ac5c37bbf054b5af7d9f47e0761147c53
Chippix <sockyadad@calypso.com>
2026-02-10 14:11:13 +0100

    implement DACGen framework

---
+++ DACStatement.cpp
@@ -0,0 +1,29 @@
+#include "DACStatement.h"
+
+namespace MIEC {
+
+	DACStatement::DACStatement(Operand const& firstOp, Operand const& secondOp, TOpKind const op) :
+		mFirstOperand(firstOp), mSecondOperand(secondOp), mOpperation(op)
+	{
+	}
+
+	DACStatement::DACStatement(Operand const& firstOp, TOpKind const op) :
+		mFirstOperand(firstOp), mSecondOperand(firstOp), mOpperation(op)
+	{
+	}
+
+	Operand DACStatement::GetFirst()
+	{
+		return mFirstOperand;
+	}
+
+	Operand DACStatement::GetSecond()
+	{
+		return mSecondOperand;
+	}
+
+	TOpKind DACStatement::GetOpType()
+	{
+		return mOpperation;
+	}
+}
